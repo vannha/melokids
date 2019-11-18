a7a8378050a8fd5e0afaeb3b4012c118fd0a7ddb
vannha <phongnhatk21@gmail.com>
2019-11-18 15:13:28 +0700

    commit -

---
+++ inc/woo/js/woo-filter.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+    "use strict";
+    $('.zk-main').each(function(){
+        var $this = $(this),
+            $id = $(this).attr('id'),
+            $loading_class = 'loading';
+        $this.find('.wc-layered-nav-term > a').live('click',function(){
+            console.log('xxx');
+            $this.fadeTo('slow',0.3).addClass($loading_class);
+            var $link = $(this).attr('href');
+            window.history.pushState({url: "" + $link + ""}, "", $link);
+            jQuery.get($link,function(data){
+                $this.html($(data).find('#'+$id).html());
+                $this.fadeTo('slow',1).removeClass($loading_class);
+            });
+            //jQuery('audio,video').mediaelementplayer();
+            return false;
+        });
+    })
+});
