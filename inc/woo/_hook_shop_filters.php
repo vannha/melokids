3c8803554d1131a48bc350d80681ffbd101602c7
vannha <phongnhatk21@gmail.com>
2020-06-12 09:15:46 +0700

    commit -

--- inc/woo/_hook_shop_filters.php
+++ inc/woo/_hook_shop_filters.php
@@ -123,7 +123,7 @@ if ( ! function_exists( 'melokids_woocommerce_catalog_ordering_filter' ) ) {
 
                         $link = melokids_get_shop_page_link( wc_get_page_id( 'shop' ));
 
-                        //$link = add_query_arg( 'orderby', $id, $link );
+                        $link = add_query_arg( 'orderby', $id, $link );
 
                         ?>
                         
