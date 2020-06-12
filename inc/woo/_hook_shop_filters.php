99fcedb38a4052601ee14b3189fc68ead7c24d00
vannha <phongnhatk21@gmail.com>
2020-06-12 09:31:50 +0700

    commit -

--- inc/woo/_hook_shop_filters.php
+++ inc/woo/_hook_shop_filters.php
@@ -73,6 +73,8 @@ function melokids_get_shop_page_link( $keep_query = false ) {
         $link = get_term_link( get_query_var( 'product_cat' ), 'product_cat' );
     } elseif ( is_product_tag() ) {
         $link = get_term_link( get_query_var( 'product_tag' ), 'product_tag' );
+    } elseif(get_query_var( 'term' ) && get_query_var( 'taxonomy' )) {
+        $link = get_term_link( get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
     } else {
         $link = get_term_link( get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
     }
@@ -87,6 +89,67 @@ function melokids_get_shop_page_link( $keep_query = false ) {
             }
 
             $link = add_query_arg( $key, $val, $link );
+        }
+    }
+
+    return $link;
+}
+function melokids_get_current_page_url() {
+    if ( defined( 'SHOP_IS_ON_FRONT' ) ) {
+        $link = home_url();
+    } elseif ( is_shop() ) {
+        $link = get_permalink( wc_get_page_id( 'shop' ) );
+    } elseif ( is_product_category() ) {
+        $link = get_term_link( get_query_var( 'product_cat' ), 'product_cat' );
+    } elseif ( is_product_tag() ) {
+        $link = get_term_link( get_query_var( 'product_tag' ), 'product_tag' );
+    } else {
+        $queried_object = get_queried_object();
+        $link = get_term_link( $queried_object->slug, $queried_object->taxonomy );
+    }
+
+    // Min/Max.
+    if ( isset( $_GET['min_price'] ) ) {
+        $link = add_query_arg( 'min_price', wc_clean( wp_unslash( $_GET['min_price'] ) ), $link );
+    }
+
+    if ( isset( $_GET['max_price'] ) ) {
+        $link = add_query_arg( 'max_price', wc_clean( wp_unslash( $_GET['max_price'] ) ), $link );
+    }
+
+    // Order by.
+    if ( isset( $_GET['orderby'] ) ) {
+        $link = add_query_arg( 'orderby', wc_clean( wp_unslash( $_GET['orderby'] ) ), $link );
+    }
+
+    /**
+     * Search Arg.
+     * To support quote characters, first they are decoded from &quot; entities, then URL encoded.
+     */
+    if ( get_search_query() ) {
+        $link = add_query_arg( 's', rawurlencode( wp_specialchars_decode( get_search_query() ) ), $link );
+    }
+
+    // Post Type Arg.
+    if ( isset( $_GET['post_type'] ) ) {
+        $link = add_query_arg( 'post_type', wc_clean( wp_unslash( $_GET['post_type'] ) ), $link );
+    }
+
+    // Min Rating Arg.
+    if ( isset( $_GET['rating_filter'] ) ) {
+        $link = add_query_arg( 'rating_filter', wc_clean( wp_unslash( $_GET['rating_filter'] ) ), $link );
+    }
+
+    // All current filters.
+    if ( $_chosen_attributes = WC_Query::get_layered_nav_chosen_attributes() ) { // phpcs:ignore Squiz.PHP.DisallowMultipleAssignments.Found, WordPress.CodeAnalysis.AssignmentInCondition.Found
+        foreach ( $_chosen_attributes as $name => $data ) {
+            $filter_name = sanitize_title( str_replace( 'pa_', '', $name ) );
+            if ( ! empty( $data['terms'] ) ) {
+                $link = add_query_arg( 'filter_' . $filter_name, implode( ',', $data['terms'] ), $link );
+            }
+            if ( 'or' === $data['query_type'] ) {
+                $link = add_query_arg( 'query_type_' . $filter_name, 'or', $link );
+            }
         }
     }
 
@@ -121,9 +184,9 @@ if ( ! function_exists( 'melokids_woocommerce_catalog_ordering_filter' ) ) {
                     <?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
                         <?php
 
-                        $link = melokids_get_shop_page_link( wc_get_page_id( 'shop' ));
-var_dump($link);
-                        $link = add_query_arg( 'orderby', $id, $link );
+                        //$link = melokids_get_shop_page_link( wc_get_page_id( 'shop' ));
+                        //$link = add_query_arg( 'orderby', $id, $link );
+                        $link = melokids_get_current_page_url();
 
                         ?>
                         <li>
